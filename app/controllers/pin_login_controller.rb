8eef31eeaac34cb7d2049fbe4194d184ec628e22
Daniel Guettler <daniel.guettler@gmail.com>
2010-10-25 16:41:35 -0400

    Harvard PIN - set 'preferences' cookie for harvard_pin

--- app/controllers/pin_login_controller.rb
+++ app/controllers/pin_login_controller.rb
@@ -1,6 +1,7 @@
 class PinLoginController < ApplicationController
 
   skip_before_filter :require_login, :authentication, :authorization
+  before_filter :set_harvard_pin_preference
 
   def validate
     @session = PinSession.new(params)
@@ -15,5 +16,11 @@ class PinLoginController < ApplicationController
 
   def access_denied
   end
+
+  protected
+    def set_harvard_pin_preference
+      preferences = ActiveSupport::JSON.decode(cookies['preferences'] || '{}')
+      cookies['preferences'] = ActiveSupport::JSON.encode(preferences.merge!({ 'harvard_pin' => true }))
+    end
 
 end
